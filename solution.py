46fec55e72258d0a3cd53f29fc20e2a47d22e941
Gleb Potapov <122749679+PotapovGN@users.noreply.github.com>
2023-04-06 23:51:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,14 +3,14 @@ import numpy as np
 
 from scipy.stats import norm
 
-
 chat_id = 485082255 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    m = x.mean()
-    left = ( (m - 0.047) / (1 - (alpha / 2)) ) + 0.047
-    right = ( (m - 0.047) / (alpha / 2) ) + 0.047
-    return (left, right)
 
+    k = np.ceil(p * len(x)).astype(int)
+    alpha_quantile = np.sort(x)[k]
+
+    left = 0.047
+    right = alpha_quantile
+    return (left, right)
 
